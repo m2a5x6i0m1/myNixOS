99a18ca85939070e46c750317ca87fc0f9ead25f
m2a5x6i0m1 <max25601new@gmail.com>
2025-07-29 19:24:57 +0300

    fix issues with suspend/hibernate

--- modules/nixos/system/graphics.nix
+++ modules/nixos/system/graphics.nix
@@ -8,4 +8,7 @@
   # Nvidia
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;
+
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
 }
